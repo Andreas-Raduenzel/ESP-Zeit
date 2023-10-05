cdae78bb7e35d2ea4de9720909c02e6993392890
raa <andreasraduenzel@yahoo.de>
2023-10-05 18:42:31 +0200

    sec commit Zeit ausgeben auf 4digtit_Max7219

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
 #include <ESP8266WiFi.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
-
-#include "LedControl.h"
+#include <LedControl.h> // Für die Ansteuerung des MAX7219
 
 const char* ssid = "FritzRa";
 const char* password = "07149590308496325159";
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org");
+
+// Die Parameter sind (DIN, CLK, CS, Anzahl der Displays)
+LedControl lc=LedControl(12,11,10,1); 
 
 void setup() {
   Serial.begin(115200);
@@ -20,16 +22,27 @@ void setup() {
   }
 
   timeClient.begin();
-  timeClient.setTimeOffset(7200);  // Anpassen Sie die Zeitzone nach Bedarf
+  timeClient.setTimeOffset(7200); 
+
+  // Initialisiere das MAX7219 Display
+  lc.shutdown(0,false);  // Schalte den Energiesparmodus aus
+  lc.setIntensity(0,8);  // Setze die Helligkeit (0 min, 15 max)
+  lc.clearDisplay(0);    // Lösche das Display
 }
 
 void loop() {
   timeClient.update();
-  timeClient.getHours();
-  
+
+  int hours = timeClient.getHours();
+  int minutes = timeClient.getMinutes();
 
   Serial.println(timeClient.getFormattedTime());
 
-
-  delay(1000);  // Warten Sie 1 Sekunde, bevor Sie die Uhrzeit erneut aktualisieren
+  // Zeige die Zeit auf dem MAX7219 an
+  lc.setDigit(0, 0, hours / 10, false); // Erste Ziffer der Stunden
+  lc.setDigit(0, 1, hours % 10, true);  // Zweite Ziffer der Stunden (mit Dezimalpunkt als Trenner)
+  lc.setDigit(0, 2, minutes / 10, false); // Erste Ziffer der Minuten
+  lc.setDigit(0, 3, minutes % 10, false); // Zweite Ziffer der Minuten
+  
+  delay(1000);
 }
